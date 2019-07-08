bb0f4f0fdc768cb41be3d2010614b7203c09d6fc
Mirian <mmulatti78@gmail.com>
2019-07-08 11:21:37 -0300

    Fizemos algumas telas - Login e cadEmp (arrumar posiçãos)

---
+++ app/src/main/java/com/example/aluno/projetoandroid/CadEmp.java
@@ -0,0 +1,13 @@
+package com.example.aluno.projetoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadEmp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_emp);
+    }
+}
