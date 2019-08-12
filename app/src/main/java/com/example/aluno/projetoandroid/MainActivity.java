fd1f73c9287c3c5d18c6589bcddfd1fc03be8c63
taissabianca7@gmail.com <taissabianca3180>
2019-08-12 11:22:18 -0300

    Alteramos a tela cadastro do restaurante, falta a outra tela. Conecção com as outras telas também

--- app/src/main/java/com/example/aluno/projetoandroid/MainActivity.java
+++ app/src/main/java/com/example/aluno/projetoandroid/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.aluno.projetoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button botaoCadRes;
+    private Button botaoCadCli;
+    private Button botaoLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botaoCadCli = (Button) findViewById(R.id.botaoCadCli);
+        botaoCadRes = (Button) findViewById(R.id.botaoCadRes);
+        botaoLogin = (Button) findViewById(R.id.botaoLogin);
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, TelaLogin.class);
+                startActivity(it);
+            }
+        });
+        botaoCadCli.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, CadEmp.class);
+                startActivity(it);
+            }
+        });
     }
 }
