1a0ad7705a37b4fb92eba236349c14f2683192ff
Weriky <werikyfernando@gmail.com>
2019-10-31 12:11:52 -0300

    Definição de IDs, nova classe Empresa, salvar dados.

--- app/src/main/java/com/example/aluno/projetoandroid/CadEmp.java
+++ app/src/main/java/com/example/aluno/projetoandroid/CadEmp.java
@@ -19,15 +19,65 @@ import com.orm.SugarContext;
 import com.orm.SugarDb;
 
 public class CadEmp extends AppCompatActivity {
-    private EditText nome;
-    private EditText email;
-    private Button btn;
+
     private ImageView ImageViewFoto;
     private Button botaoImg;
+    private EditText nome;
+    private EditText localizacao;
+    private EditText cardapio;
+    private EditText telefone;
+    private EditText email;
+    private EditText senha1;
+    private EditText senha2;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_emp);
+
+        nome = (EditText) findViewById(R.id.editTextNome2);
+        localizacao = (EditText) findViewById(R.id.editTextLoca);
+        cardapio = (EditText) findViewById(R.id.editTextCar);
+        telefone = (EditText) findViewById(R.id.editTextTel);
+        email = (EditText) findViewById(R.id.editTextEmail);
+        senha1 = (EditText) findViewById(R.id.editTextSenha1);
+        senha2 = (EditText) findViewById(R.id.editTextSenha2);
+        btn = (Button) findViewById(R.id.buttonCadastrar);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //receber os dados dos campos de texto
+                String n = nome.getText().toString();
+                String l = localizacao.getText().toString();
+                String c = cardapio.getText().toString();
+                String t = telefone.getText().toString();
+                String em = email.getText().toString();
+                String s1 = senha1.getText().toString();
+                String s2 = senha2.getText().toString();
+
+                Empresa e = new Empresa();
+                e.setNome(n);
+                e.setCardapio(c);
+                e.setTelefone(t);
+                e.setEmail(em);
+                e.setSenha1(s1);
+                e.setSenha2(s2);
+
+                //salvando
+                e.save();
+
+                nome.setText("");
+                email.setText("");
+                localizacao.setText("");
+                cardapio.setText("");
+                telefone.setText("");
+                senha1.setText("");
+                senha2.setText("");
+
+            }
+        });
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
