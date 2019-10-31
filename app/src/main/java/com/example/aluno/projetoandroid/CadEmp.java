d75e0c1a7918095f09caf328c2319fc60306f22d
Mirian <mmulatti78@gmail.com>
2019-10-31 12:01:10 -0300

    Camera concluida

--- app/src/main/java/com/example/aluno/projetoandroid/CadEmp.java
+++ app/src/main/java/com/example/aluno/projetoandroid/CadEmp.java
@@ -1,11 +1,17 @@
 package com.example.aluno.projetoandroid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.orm.SchemaGenerator;
@@ -16,12 +22,24 @@ public class CadEmp extends AppCompatActivity {
     private EditText nome;
     private EditText email;
     private Button btn;
+    private ImageView ImageViewFoto;
+    private Button botaoImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_emp);
 
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
 
+        ImageViewFoto = (ImageView) findViewById(R.id.imageView2);
+        findViewById(R.id.botaoImg).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tirarFoto();
+            }
+        });
         //depois que rodou uma vez comenta essas 3 linhas
         //SugarContext.init(getApplicationContext());
         //SchemaGenerator schemaGenerator = new SchemaGenerator(this);
@@ -52,5 +70,20 @@ public class CadEmp extends AppCompatActivity {
             }
         });
 
+    }
+    public void tirarFoto(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imagem = (Bitmap) extras.get("data");
+            ImageViewFoto.setImageBitmap(imagem);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
